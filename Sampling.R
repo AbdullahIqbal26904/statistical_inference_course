6671aa003e764bc3cd77a0938d335765eea1b85e
Abdullah Iqbal <75801854+AbdullahIqbal26904@users.noreply.github.com>
2024-04-20 23:25:13 +0500

    Add files via upload

---
+++ Sampling.R
@@ -0,0 +1,12 @@
+
+x=1:50
+x
+samplex=sample(x, size=10) # default is without replacement
+samplex
+samplex=sample(x, size=10, replace=T) # with replacement
+samplex # will display selected students SN numbers
+
+y<-sample(c("Amir", "Faisal","Fahad", "Junaid", "Ali", "Umar","Razi"),size=1)
+y
+
+ 
